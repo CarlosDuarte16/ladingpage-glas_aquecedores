9a5967348eb690cf3732551a183058bf7e9043f4
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2024-11-29 14:54:59 -0300

    Rotas

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,10 @@ import './index.css';
 import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 import Home from './pages/home/Index';
 import { Toaster } from 'react-hot-toast';
-
+import Products_Heaters from '.pages/products-aquecedores/Index';
+import Products_Kits from './pages/products-kits/Index';
+import Products_Accessories from './pages/products-acessórios/Index';
+import Products_Pressurizers from './pages/products-pressurizadores/Index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +15,10 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='aquecedores' element={<Products_Heaters/>}/>
+        <Route path='kits' element={<Products_Kits/>}/>
+        <Route path='acessórios' element={<Products_Accessories/>}/>
+        <Route path='pressurizadores' element={<Products_Pressurizers/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
