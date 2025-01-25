ac54b414b37852631e3a6ecfd93eacbabef06953
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2025-01-25 20:00:46 -0300

    Atualização card services

--- src/components/Card_services/Index.jsx
+++ src/components/Card_services/Index.jsx
@@ -1,7 +1,8 @@
 import './Index.scss';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 
-export default function CardServices({image, title, text, button}){
+
+export default function CardServices({ image, title, text, button }) {
 
   function whatsapp(){
     window.location.href = 'https://wa.me/5511947398728?text=Ol%C3%A1,%20estou%20precisando%20de%20um%20or%C3%A7amento.%20Pode%20ajudar?';
@@ -9,10 +10,16 @@ export default function CardServices({image, title, text, button}){
 
   return (
     <div className="comp-card_services">
-      <img src={image} alt="" />
+      <div onClick={whatsapp} className="pop-up">
+        <div className="image-box">
+          <img src={image} alt="" />
+        </div>
+        <div  className="content">
+          <h3>Clique aqui para fazer seu orçamento!</h3>
+        </div>
+      </div>
       <h2>{title}</h2>
       <p>{text}</p>
-      <button onClick={whatsapp}>{button}</button>
     </div>
   );
 }
